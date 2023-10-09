0eebc9ee79310af9220ad57ac9d71bd4dbab3cf9
Bagasseno <bagasseno77@gmail.com>
2023-10-10 05:43:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,22 +9,32 @@ app.use(bodyParser.raw())
 app.use(bodyParser.json())
 
 
-
 app.post('/user', (req, res) => {
-    const { email, password, confirmPassword, username} = req.body
-    console.log(req.body)
-    const sql = `INSERT INTO users (email, password, confirm_Password, username) VALUES 
-    ('${email}', '${password}', '${confirmPassword}', '${username}')`
-    db.query(sql, (error, result) => {
+    const { email, password, confirm_Password, username} = req.body
+    const sql = `INSERT INTO users (email, password, confirmPassword, username) VALUES 
+    ('${email}', '${password}', '${confirm_Password}', '${username}')`
+    db.query(sql, (error, fields) => {
         if (error) console.log(error)
-        console.log(result)
+        console.log(fields)
     })
-    res.send("ok")
 })
 
 
+app.post('/chats', (req, res) => {
+    const { Nama, Keluhan, Usia_Kehamilan, Riwayat_Penyakit, Pembayaran_Via } = req.body
+    const sql = `INSERT INTO chats (Nama, Keluhan, Usia_kehamilan, Riwayat_Penyakit, Pembayaran_Via) VALUES 
+    ('${Nama}', '${Keluhan}', '${Usia_Kehamilan}', '${Riwayat_Penyakit}', '${Pembayaran_Via}')`
+    db.query(sql, (err, fields)=>{
+        if(err) {
+            console.log(err)
+            res.status(500).json({ msg : "invalid"})
+        } else {
+            res.json(fields)
+        }
+    })
+})
 
 
-app.listen(5000, () => {
+app.listen(3900, () => {
     console.log('port berjalan...')
 })
