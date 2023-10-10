c702bf9f7720cb6ab3167c6d21f3963d7c01b4cb
Bagasseno <bagasseno77@gmail.com>
2023-10-10 12:49:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,19 +8,23 @@ app.use(express.json())
 app.use(bodyParser.raw())
 app.use(bodyParser.json())
 
-
+// endpoint signUp
 app.post('/user', (req, res) => {
     const { email, password, confirm_Password, username} = req.body
     const sql = `INSERT INTO users (email, password, confirmPassword, username) VALUES 
     ('${email}', '${password}', '${confirm_Password}', '${username}')`
     db.query(sql, (error, fields) => {
-        if (error) console.log(error)
-        console.log(fields)
+        if (error) {
+            console.log(error)
+            res.status(500).json({ msg : "invalid"})
+        } else {
+            res.json(fields)
+        }
     })
 })
 
-
-app.post('/chats', (req, res) => {
+// endpoint chat consultation
+app.post('/chat', (req, res) => {
     const { Nama, Keluhan, Usia_Kehamilan, Riwayat_Penyakit, Pembayaran_Via } = req.body
     const sql = `INSERT INTO chats (Nama, Keluhan, Usia_kehamilan, Riwayat_Penyakit, Pembayaran_Via) VALUES 
     ('${Nama}', '${Keluhan}', '${Usia_Kehamilan}', '${Riwayat_Penyakit}', '${Pembayaran_Via}')`
